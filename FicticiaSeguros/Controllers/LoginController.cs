c8644331eb88b385e230f741d94658c9b1d1e5de
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-07 00:54:11 -0300

    Login Controler

--- FicticiaSeguros/Controllers/LoginController.cs
+++ FicticiaSeguros/Controllers/LoginController.cs
@@ -1,6 +1,10 @@
 ﻿using FicticiaSeguros.Models;
 using FicticiaSeguros.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FicticiaSeguros.Controllers
 {
@@ -16,9 +20,55 @@ namespace FicticiaSeguros.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            user.UserPass = Utils.HashPassword(user.UserPass);
+
+            User userCreated = await _userService.SaveUser(user);
+
+            if (userCreated.Id > 0 )
+            {
+                return RedirectToAction("SignIn","Login");
+            }
+            ViewData["Message"] = "Error creating user";
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SignIn(string mail, string pass)
+        {
+            User userFinded = await _userService.GetUser(mail, Utils.HashPassword(pass));
+            if (userFinded == null)
+            {
+                ViewData["Message"] = "User not found";
+                return View();
+            }
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, userFinded.UserName),
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                properties
+                );
+            return RedirectToAction("Index", "Home");
         }
     }
 }
